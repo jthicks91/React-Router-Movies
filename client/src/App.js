1e7872e3c7d9ed083190e80a68763f4fbdbf5eac
jthicks91 <jordanthicks91@gmail.com>
2019-03-26 22:12:42 -0700

    completed part 1 of project by rending the list of movies to the screen. Next will be making them links.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import SavedList from './Movies/SavedList';
-import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie';
+import SavedList from "./Movies/SavedList";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 export default class App extends Component {
   constructor() {
@@ -23,6 +25,8 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         <div>Replace this Div with your Routes</div>
+        <Route path="/" component={MovieList} />
+        <Route path="/movies/:id" component={Movie} />
       </div>
     );
   }
