4ac7c69073110d802ee601a8be19e99b2691ce7d
jthicks91 <jordanthicks91@gmail.com>
2019-03-27 10:14:16 -0700

    MVP Complete.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -11,8 +11,7 @@ export default class Movie extends Component {
 
   componentDidMount() {
     // change this line to grab the id passed on the URL
-    const { id } = this.props.match.params;
-    this.fetchMovie(id);
+    this.fetchMovie(this.props.match.params.id);
   }
 
   fetchMovie = id => {
