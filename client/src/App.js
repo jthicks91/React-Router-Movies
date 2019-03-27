d606dc9be45540c1cb12f5194efedf1fe8a040f1
jthicks91 <jordanthicks91@gmail.com>
2019-03-26 22:34:15 -0700

    working to complete part 2 of project. currently stuck on making card a link that directs user to the URL of the individual movie.

--- client/src/App.js
+++ client/src/App.js
@@ -24,8 +24,7 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
-        <Route path="/" component={MovieList} />
+        <Route exact path="/" component={MovieList} />
         <Route path="/movies/:id" component={Movie} />
       </div>
     );
