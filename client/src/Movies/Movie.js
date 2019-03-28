086f5757948a6581f88602ab087044055268b7f8
jthicks91 <jordanthicks91@gmail.com>
2019-03-27 18:17:51 -0700

    forgot to add functionality for home button. added and working.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -26,23 +26,23 @@ export default class Movie extends Component {
       });
   };
   // Uncomment this code when you're ready for the stretch problems
-  // componentWillReceiveProps(newProps){
-  //   if(this.props.match.params.id !== newProps.match.params.id){
-  //     this.fetchMovie(newProps.match.params.id);
-  //   }
-  // }
 
-  // saveMovie = () => {
-  //   const addToSavedList = this.props.addToSavedList;
-  //   addToSavedList(this.state.movie)
-  // }
+  componentWillReceiveProps(newProps) {
+    if (this.props.match.params.id !== newProps.match.params.id) {
+      this.fetchMovie(newProps.match.params.id);
+    }
+  }
+
+  saveMovie = () => {
+    const addToSavedList = this.props.addToSavedList;
+    addToSavedList(this.state.movie);
+  };
 
   render() {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
     }
 
-    // const { title, director, metascore, stars } = this.state.movie;
     return (
       <MovieCard
         key={this.props.key}
